33da160c7ea6bcbc2c6764dcefa4b91f19e299ba
Pavle Simonovic <pavsimono@Pavles-MacBook-Pro.local>
2020-02-19 19:35:50 -0600

    adding vars to Tile class

--- src/Tile.hpp
+++ src/Tile.hpp
@@ -10,9 +10,22 @@
 
 #include <stdio.h>
 
+#include <vector>
+
+#include <glm/mat3x3.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
 class Tile
 {
+public:
+    Tile(std::vector<glm::vec2> vertices,
+         std::vector<glm::mat3x3> T_mats) : vertices(vertices), T_mats(T_mats) { };
     
+private:
+    
+    std::vector<glm::vec2> vertices;
+    std::vector<glm::mat3x3> T_mats;
 };
 
 #endif /* Tile_hpp */
